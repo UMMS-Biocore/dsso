2f0479d69c6c3bc237c7aba06af66bb75420e990
Onur Yukselen <onuryukselen@gmail.com>
2020-10-20 11:29:41 -0400

    webpack configured and coreui template imported

--- public/js/index.js
+++ public/js/index.js
@@ -1,23 +1,20 @@
 /* eslint-disable */
 import '@babel/polyfill';
-import { displayMap } from './mapbox';
 import { logout } from './login';
 import { updateSettings } from './updateSettings';
 import { showAlert } from './alerts';
+// require('jquery');
+// require('popper.js');
+// require('pace');
+// require('perfect-scrollbar');
+// require('@coreui/coreui');
+// require('chart.js');
 
 // DOM ELEMENTS
-const mapBox = document.getElementById('map');
 // const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
-const bookBtn = document.getElementById('book-tour');
-
-// DELEGATION
-if (mapBox) {
-  const locations = JSON.parse(mapBox.dataset.locations);
-  displayMap(locations);
-}
 
 // if (loginForm)
 //   loginForm.addEventListener('submit', e => {
@@ -54,12 +51,6 @@ if (userPasswordForm)
     document.getElementById('password-current').value = '';
     document.getElementById('password').value = '';
     document.getElementById('password-confirm').value = '';
-  });
-
-if (bookBtn)
-  bookBtn.addEventListener('click', e => {
-    e.target.textContent = 'Processing...';
-    const { tourId } = e.target.dataset;
   });
 
 const alertMessage = document.querySelector('body').dataset.alert;
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path'); //output tanimlarken absolute path kullnilmasi gerekiyor
+const TerserPlugin = require('terser-webpack-plugin'); //minimize js content
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: { bundle: './public/js/index.js', 'styles-libs': './public/scss/styles-libs.scss' },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, './public/dist'), //create dist directory and save it bundle.js
+    publicPath: ''
+  },
+  watch: true,
+  watchOptions: {
+    ignored: ['node_modules/**']
+  },
+  mode: 'development',
+  module: {
+    // how to import files
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader' // once css-loader calisir sonra style-loader
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'] //convert ES6 to support old browsers
+      }
+    ]
+  },
+  plugins: [
+    new TerserPlugin(), // minimize js content
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    }), // extract css into separate file
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ['public/dist']
+    }) // removes unused files in dist folder
+  ]
+};
